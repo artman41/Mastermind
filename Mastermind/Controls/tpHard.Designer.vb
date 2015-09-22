1bd0b33a452dfea37dbd7bbc7a42fa79a939fced
artman41 <artman41@gmail.com>
2015-09-22 11:09:38 +0100

    Fixed Hard tab name reading as Medium

--- Mastermind/Controls/tpHard.Designer.vb
+++ Mastermind/Controls/tpHard.Designer.vb
@@ -22,9 +22,6 @@ Partial Class tpHard
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.SuspendLayout()
-        Me.ResumeLayout(False)
-
         Me.Cracker1 = New System.Windows.Forms.NumericUpDown()
         Me.Cracker2 = New System.Windows.Forms.NumericUpDown()
         Me.Cracker3 = New System.Windows.Forms.NumericUpDown()
@@ -34,15 +31,95 @@ Partial Class tpHard
         Me.ButtonCheck = New System.Windows.Forms.Button()
         Me.CrackerProgress = New System.Windows.Forms.ProgressBar()
         Me.TimesChecked = New System.Windows.Forms.Label()
-
         CType(Me.Cracker1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Cracker2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Cracker3, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Cracker4, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Cracker5, System.ComponentModel.ISupportInitialize).BeginInit()
-
+        Me.SuspendLayout()
         '
-        'Tab
+        'Cracker1
+        '
+        Me.Cracker1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Cracker1.Location = New System.Drawing.Point(6, 60)
+        Me.Cracker1.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
+        Me.Cracker1.Name = "Cracker1"
+        Me.Cracker1.Size = New System.Drawing.Size(44, 38)
+        Me.Cracker1.TabIndex = 0
+        '
+        'Cracker2
+        '
+        Me.Cracker2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Cracker2.Location = New System.Drawing.Point(69, 60)
+        Me.Cracker2.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
+        Me.Cracker2.Name = "Cracker2"
+        Me.Cracker2.Size = New System.Drawing.Size(44, 38)
+        Me.Cracker2.TabIndex = 1
+        '
+        'Cracker3
+        '
+        Me.Cracker3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Cracker3.Location = New System.Drawing.Point(133, 60)
+        Me.Cracker3.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
+        Me.Cracker3.Name = "Cracker3"
+        Me.Cracker3.Size = New System.Drawing.Size(44, 38)
+        Me.Cracker3.TabIndex = 2
+        '
+        'Cracker4
+        '
+        Me.Cracker4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Cracker4.Location = New System.Drawing.Point(197, 60)
+        Me.Cracker4.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
+        Me.Cracker4.Name = "Cracker4"
+        Me.Cracker4.Size = New System.Drawing.Size(44, 38)
+        Me.Cracker4.TabIndex = 3
+        '
+        'Cracker5
+        '
+        Me.Cracker5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Cracker5.Location = New System.Drawing.Point(258, 60)
+        Me.Cracker5.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
+        Me.Cracker5.Name = "Cracker5"
+        Me.Cracker5.Size = New System.Drawing.Size(44, 38)
+        Me.Cracker5.TabIndex = 8
+        '
+        'ButtonCrack
+        '
+        Me.ButtonCrack.Location = New System.Drawing.Point(6, 213)
+        Me.ButtonCrack.Name = "ButtonCrack"
+        Me.ButtonCrack.Size = New System.Drawing.Size(297, 23)
+        Me.ButtonCrack.TabIndex = 4
+        Me.ButtonCrack.Text = "Crack"
+        Me.ButtonCrack.UseVisualStyleBackColor = True
+        '
+        'ButtonCheck
+        '
+        Me.ButtonCheck.Enabled = False
+        Me.ButtonCheck.Location = New System.Drawing.Point(7, 155)
+        Me.ButtonCheck.Name = "ButtonCheck"
+        Me.ButtonCheck.Size = New System.Drawing.Size(296, 23)
+        Me.ButtonCheck.TabIndex = 5
+        Me.ButtonCheck.Text = "Check"
+        Me.ButtonCheck.UseVisualStyleBackColor = True
+        '
+        'CrackerProgress
+        '
+        Me.CrackerProgress.Location = New System.Drawing.Point(6, 6)
+        Me.CrackerProgress.Name = "CrackerProgress"
+        Me.CrackerProgress.Size = New System.Drawing.Size(296, 23)
+        Me.CrackerProgress.TabIndex = 6
+        '
+        'TimesChecked
+        '
+        Me.TimesChecked.AutoSize = True
+        Me.TimesChecked.Location = New System.Drawing.Point(112, 115)
+        Me.TimesChecked.Name = "TimesChecked"
+        Me.TimesChecked.Size = New System.Drawing.Size(81, 13)
+        Me.TimesChecked.TabIndex = 7
+        Me.TimesChecked.Text = "Times Checked"
+        Me.TimesChecked.Visible = False
+        '
+        'tpHard
         '
         Me.Controls.Add(Me.TimesChecked)
         Me.Controls.Add(Me.CrackerProgress)
@@ -57,99 +134,15 @@ Partial Class tpHard
         Me.Name = "TabMedium"
         Me.Padding = New System.Windows.Forms.Padding(3)
         Me.Size = New System.Drawing.Size(309, 242)
-        Me.TabIndex = 0
-        Me.Text = "Medium"
+        Me.Text = "Hard"
         Me.UseVisualStyleBackColor = True
-        '
-        'Cracker5
-        '
-        Me.Cracker5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Cracker5.Location = New System.Drawing.Point(258, 60)
-        Me.Cracker5.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
-        Me.Cracker5.Name = "Cracker5"
-        Me.Cracker5.Size = New System.Drawing.Size(44, 38)
-        Me.Cracker5.TabIndex = 8
-        '
-        'TimesChecked
-        '
-        Me.TimesChecked.Visible = False
-        Me.TimesChecked.AutoSize = True
-        Me.TimesChecked.Location = New System.Drawing.Point(112, 115)
-        Me.TimesChecked.Name = "TimesChecked"
-        Me.TimesChecked.Size = New System.Drawing.Size(81, 13)
-        Me.TimesChecked.TabIndex = 7
-        Me.TimesChecked.Text = "Times Checked"
-        '
-        'CrackerProgress
-        '
-        Me.CrackerProgress.Location = New System.Drawing.Point(6, 6)
-        Me.CrackerProgress.Name = "CrackerProgress"
-        Me.CrackerProgress.Size = New System.Drawing.Size(296, 23)
-        Me.CrackerProgress.TabIndex = 6
-        '
-        'ButtonCheck
-        '
-        Me.ButtonCheck.Enabled = False
-        Me.ButtonCheck.Location = New System.Drawing.Point(7, 155)
-        Me.ButtonCheck.Name = "ButtonCheck"
-        Me.ButtonCheck.Size = New System.Drawing.Size(296, 23)
-        Me.ButtonCheck.TabIndex = 5
-        Me.ButtonCheck.Text = "Check"
-        Me.ButtonCheck.UseVisualStyleBackColor = True
-        '
-        'ButtonCrack
-        '
-        Me.ButtonCrack.Location = New System.Drawing.Point(6, 213)
-        Me.ButtonCrack.Name = "ButtonCrack"
-        Me.ButtonCrack.Size = New System.Drawing.Size(297, 23)
-        Me.ButtonCrack.TabIndex = 4
-        Me.ButtonCrack.Text = "Crack"
-        Me.ButtonCrack.UseVisualStyleBackColor = True
-        '
-        'Cracker4
-        '
-        Me.Cracker4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Cracker4.Location = New System.Drawing.Point(197, 60)
-        Me.Cracker4.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
-        Me.Cracker4.Name = "Cracker4"
-        Me.Cracker4.Size = New System.Drawing.Size(44, 38)
-        Me.Cracker4.TabIndex = 3
-        '
-        'Cracker3
-        '
-        Me.Cracker3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Cracker3.Location = New System.Drawing.Point(133, 60)
-        Me.Cracker3.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
-        Me.Cracker3.Name = "Cracker3"
-        Me.Cracker3.Size = New System.Drawing.Size(44, 38)
-        Me.Cracker3.TabIndex = 2
-        '
-        'Cracker2
-        '
-        Me.Cracker2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Cracker2.Location = New System.Drawing.Point(69, 60)
-        Me.Cracker2.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
-        Me.Cracker2.Name = "Cracker2"
-        Me.Cracker2.Size = New System.Drawing.Size(44, 38)
-        Me.Cracker2.TabIndex = 1
-        '
-        'Cracker1
-        '
-        Me.Cracker1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Cracker1.Location = New System.Drawing.Point(6, 60)
-        Me.Cracker1.Maximum = New Decimal(New Integer() {9, 0, 0, 0})
-        Me.Cracker1.Name = "Cracker1"
-        Me.Cracker1.Size = New System.Drawing.Size(44, 38)
-        Me.Cracker1.TabIndex = 0
-        '
-        'Temp
-        '
-        CType(Me.Cracker5, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Cracker4, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Cracker3, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Cracker2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Cracker1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Cracker2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Cracker3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Cracker4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Cracker5, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
